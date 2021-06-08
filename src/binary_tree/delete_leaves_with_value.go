972e866ee77dd434976b031b0f80ca5b47d0cea6
Александр Валов <a.valov@sports.ru>
2021-06-08 13:48:16 +0300

    DELETE_LEAVES_WITH_VALUE descrition added

--- src/binary_tree/delete_leaves_with_value.go
+++ src/binary_tree/delete_leaves_with_value.go
@@ -1,3 +1,8 @@
+//Given a binary tree root and an integer target, delete all the leaf nodes with value target.
+//
+//Note that once you delete a leaf node with value target,
+//if it's parent node becomes a leaf node and has the value target,
+//it should also be deleted (you need to continue doing that until you can't).
 package binary_tree
 
 type TreeNode struct {

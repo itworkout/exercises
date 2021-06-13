a68ff2801fab803440d7d3ed787533ec34ab672f
Александр Валов <a.valov@sports.ru>
2021-06-13 23:15:42 +0300

    NTH_HIGHEST_SALARY solution (sql query)

--- src/binary_tree/delete_leaves_with_value.go
+++ src/binary_tree/delete_leaves_with_value.go
@@ -1,3 +1,4 @@
+//https://leetcode.com/problems/delete-leaves-with-a-given-value/
 //Given a binary tree root and an integer target, delete all the leaf nodes with value target.
 //
 //Note that once you delete a leaf node with value target,
